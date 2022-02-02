938c4800dc22b791ffe3ecadf9bb4bd5de73c4e3
Almira M Anderson <39601411+almiracma@users.noreply.github.com>
2022-02-02 12:05:30 -0800

    set up api

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import "./App.css";
 
-function App() {
+const App = () => {
+  const APP_ID = "8b846a17";
+  const APP_KEY = "f07831675f1f92df328804b5215215af";
+
+  const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
   return (
     <div className="App">
       <h1>Hello React</h1>
     </div>
   );
-}
+};
 
 export default App;
